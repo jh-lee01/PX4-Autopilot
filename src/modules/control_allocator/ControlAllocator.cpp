ce64b8ba134eb761facac9122a7f68e470b3bfae
Carroll Lee <carroll511@g.skku.edu>
2025-05-21 10:29:38 +0900

    feat: 2.3ver geo

--- src/modules/control_allocator/ControlAllocator.cpp
+++ src/modules/control_allocator/ControlAllocator.cpp
@@ -51,31 +51,31 @@ using namespace time_literals;
 
 // Geometry set 1: Perching
 const float ControlAllocator::values_0[12] = {
-	 0.11f,  0.19f, -0.46f,
-	-0.11f, -0.19f, -0.46f,
-	 0.11f, -0.19f, -0.46f,
-	-0.11f,  0.19f, -0.46f
+	 0.11f,  0.19f, -0.40f,
+	-0.11f, -0.19f, -0.40f,
+	 0.11f, -0.19f, -0.40f,
+	-0.11f,  0.19f, -0.40f
 };
 
 const float ControlAllocator::values_30[12] = {
-	 0.06f,  0.19f, -0.49f,
-	-0.16f, -0.19f, -0.49f,
-	 0.06f, -0.19f, -0.49f,
-	-0.16f,  0.19f, -0.49f
+	 0.07f,  0.19f, -0.40f,
+	-0.15f, -0.19f, -0.40f,
+	 0.07f, -0.19f, -0.40f,
+	-0.15f,  0.19f, -0.40f
 };
 
 const float ControlAllocator::values_60[12] = {
-	-0.22f,  0.19f, -0.20f,
-	-0.45f, -0.19f, -0.20f,
-	-0.22f, -0.19f, -0.20f,
-	-0.45f,  0.19f, -0.20f
+	-0.20f,  0.19f, -0.21f,
+	-0.43f, -0.19f, -0.21f,
+	-0.20f, -0.19f, -0.21f,
+	-0.43f,  0.19f, -0.21f
 };
 
 const float ControlAllocator::values_90[12] = {
-	-0.24f,  0.19f, -0.07f,
-	-0.46f, -0.19f, -0.07f,
-	-0.24f, -0.19f, -0.07f,
-	-0.46f,  0.19f, -0.07f
+	-0.24f,  0.19f, -0.02f,
+	-0.46f, -0.19f, -0.02f,
+	-0.24f, -0.19f, -0.02f,
+	-0.46f,  0.19f, -0.02f
 };
 
 const char *param_names[12] = {
